ee0df56bd63a7793c48ec6b39786a5f09e3d0ef2
ald2211 <afnadca2@gmail.com>
2024-05-15 03:24:01 +0530

    redux persist added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import { Store } from './Redux/Store.js'
+import { Store, persistor } from './Redux/Store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
+      <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </PersistGate>
     </Provider>
     
   </React.StrictMode>,
