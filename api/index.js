5ca2566d1764fa57b2b3a58891a26ff7f63cb717
ald2211 <afnadca2@gmail.com>
2024-05-14 13:17:02 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv  from "dotenv"
+dotenv.config()
+
+const port = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to db');
+}).catch((err)=>{
+    console.log('error:',err)
+})
 const app=express()
 
 app.get('/',(req,res)=>{
 
     res.status(200).send('hello how are you')
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('server running')
 })
